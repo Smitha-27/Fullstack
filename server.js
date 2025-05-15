33251e5c733eb7c30e2139cc5a5f8f44f0d9edd2
Smitha-27 <smithadeepakofficial@gmail.com>
2025-05-15 23:03:52 +0530

    initial commit

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Connect to MongoDB
 mongoose.connect("mongodb://localhost:27017/Fullstack", {
